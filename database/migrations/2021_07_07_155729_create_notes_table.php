c08bdf0e8a8d6539e6f3bcb8caf68bcd997d718e
alexander.aladin <alexander.aladin@smartseeds.ru>
2021-07-07 19:05:34 +0300

    feat: add migrations for notes and categories

---
+++ database/migrations/2021_07_07_155729_create_notes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('priority');
+            $table->longText('description');
+            $table->timestamp('date');
+            $table->boolean('completed')->default(false);
+            $table->unsignedBigInteger('category_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
